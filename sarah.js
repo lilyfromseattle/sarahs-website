4bc24e08a45c888e520dd7793165b1f02de6ede4
Lily Pacegit <lilyfpace@gmail.com>
2020-07-17 13:33:37 -0400

    styling

--- sarah.js
+++ sarah.js
@@ -18,3 +18,10 @@ $elements.each(function(i) {
     });
 });
 console.log("WOOOO")
+
+$(window).bind('resize', function(e)
+{
+  console.log('window resized..');
+  this.location.reload(false); /* false to get page from cache */
+  /* true to fetch page from server */
+});
